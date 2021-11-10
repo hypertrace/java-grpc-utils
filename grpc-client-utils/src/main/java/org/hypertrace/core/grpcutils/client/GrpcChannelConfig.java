eb5390c6c500cb1e0ab16cce5beab6078f50fca0
SJ <shubham@traceable.ai>
2021-11-10 14:35:16 +0530

    fix test

--- grpc-client-utils/src/main/java/org/hypertrace/core/grpcutils/client/GrpcChannelConfig.java
+++ grpc-client-utils/src/main/java/org/hypertrace/core/grpcutils/client/GrpcChannelConfig.java
@@ -2,6 +2,7 @@ package org.hypertrace.core.grpcutils.client;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode
 public class GrpcChannelConfig {
   private Integer maxInboundMessageSize;
 }
