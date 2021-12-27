967f216be3e7671951124ad7faa294dcc08c5e27
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2021-12-27 10:37:17 -0800

    feat: upgrade jackson, grpc, constrain vulns and use bom (#24)

--- grpc-server-utils/build.gradle.kts
+++ grpc-server-utils/build.gradle.kts
@@ -10,15 +10,16 @@ tasks.test {
 }
 
 dependencies {
-  api("io.grpc:grpc-context:1.42.0")
-  api("io.grpc:grpc-api:1.42.0")
+  api(platform("io.grpc:grpc-bom:1.43.1"))
+  api("io.grpc:grpc-context")
+  api("io.grpc:grpc-api")
 
   implementation(project(":grpc-context-utils"))
-  implementation("org.slf4j:slf4j-api:1.7.30")
+  implementation("org.slf4j:slf4j-api:1.7.32")
 
-  annotationProcessor("org.projectlombok:lombok:1.18.20")
-  compileOnly("org.projectlombok:lombok:1.18.20")
+  annotationProcessor("org.projectlombok:lombok:1.18.22")
+  compileOnly("org.projectlombok:lombok:1.18.22")
 
-  testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
-  testImplementation("org.mockito:mockito-core:3.12.1")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+  testImplementation("org.mockito:mockito-core:4.2.0")
 }
