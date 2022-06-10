84e5de133b52e8987a31d9859c09f3176e378347
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-06-10 15:39:31 +0200

    fix: add missing netty constraint on client utils (#30)

--- grpc-client-utils/build.gradle.kts
+++ grpc-client-utils/build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
 }
 
 dependencies {
+  constraints {
+    api("io.netty:netty-codec-http2:4.1.77.Final") {
+      because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-2812456")
+    }
+  }
   api(platform("io.grpc:grpc-bom:1.45.1"))
   api("io.grpc:grpc-context")
   api("io.grpc:grpc-api")
