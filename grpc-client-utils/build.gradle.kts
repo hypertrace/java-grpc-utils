2881279cad501de382eefa2d5a1fdb5805f2d388
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-07-26 06:17:27 -0700

    refactor: use bom to constrain netty (#32)

--- grpc-client-utils/build.gradle.kts
+++ grpc-client-utils/build.gradle.kts
@@ -6,14 +6,13 @@ plugins {
 }
 
 dependencies {
-  constraints {
-    api("io.netty:netty-codec-http2:4.1.77.Final") {
-      because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-2812456")
-    }
-  }
+
   api(platform("io.grpc:grpc-bom:1.47.0"))
   api("io.grpc:grpc-context")
   api("io.grpc:grpc-api")
+  api(platform("io.netty:netty-bom:4.1.79.Final")) {
+    because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-2812456")
+  }
 
   implementation(project(":grpc-context-utils"))
   implementation("org.slf4j:slf4j-api:1.7.36")
