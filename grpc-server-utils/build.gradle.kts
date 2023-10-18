6aba170f266d3301b71e8ff790d1d4a7c07d90d8
Aaron Steinfeld <aaron@traceable.ai>
2023-10-18 14:22:32 -0400

    fix: upgrade netty

--- grpc-server-utils/build.gradle.kts
+++ grpc-server-utils/build.gradle.kts
@@ -14,8 +14,8 @@ dependencies {
   api("io.grpc:grpc-context")
   api("io.grpc:grpc-api")
 
-  api(platform("io.netty:netty-bom:4.1.94.Final")) {
-    because("CVE-2023-34462")
+  api(platform("io.netty:netty-bom:4.1.100.Final")) {
+    because("CVE-2023-44487")
   }
 
   implementation(project(":grpc-context-utils"))
