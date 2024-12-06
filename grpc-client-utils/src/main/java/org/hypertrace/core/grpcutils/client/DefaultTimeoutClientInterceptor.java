e8afcbcc5cbf4fac6e78f709c85cde688d37e756
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2024-12-06 12:55:57 -0500

    Default timeout interceptor (#66)
    * chore: add default timeout interceptor
    
    * refactor: remove unused slf4j

---
+++ grpc-client-utils/src/main/java/org/hypertrace/core/grpcutils/client/DefaultTimeoutClientInterceptor.java
@@ -0,0 +1,32 @@
+package org.hypertrace.core.grpcutils.client;
+
+import static java.util.Objects.isNull;
+
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.Context;
+import io.grpc.MethodDescriptor;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nonnull;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class DefaultTimeoutClientInterceptor implements ClientInterceptor {
+  private final @Nonnull Duration defaultTimeout;
+
+  @Override
+  public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
+      MethodDescriptor<ReqT, RespT> methodDescriptor, CallOptions providedOptions, Channel next) {
+
+    if (isNull(providedOptions.getDeadline()) && isNull(Context.current().getDeadline())) {
+      return next.newCall(
+          methodDescriptor,
+          providedOptions.withDeadlineAfter(defaultTimeout.toMillis(), TimeUnit.MILLISECONDS));
+    }
+
+    return next.newCall(methodDescriptor, providedOptions);
+  }
+}
